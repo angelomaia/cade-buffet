5686b6a85a89feeed6768a54e0336ed590cc314c
angelomaia <request.angelo@gmail.com>
2024-04-16 12:02:28 -0300

    retrieving user model for the future

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
+  describe '#uniqueness' do
+    it 'do not sign up more than one owner per email' do
+      Owner.create!(email: 'example@email.com', password: '123456')
+      o = Owner.create(email: 'example@email.com', password: '456789')
+
+      expect(o.valid?).not_to eq true
+    end
+  end
 end
