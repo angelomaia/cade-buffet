59ff9cb863599f173aacd7dc114e4715919a0025
angelomaia <request.angelo@gmail.com>
2024-04-19 08:51:06 -0300

    Scoped devise views implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :owners
+  devise_for :users, path: 'users'
+  devise_for :owners, path: 'owners'
   root "home#index"
   get 'home/choice', to: 'home#choice'
   
   resources :buffets, only: [:show, :new, :create, :edit, :update]
   resources :event_types, only: [:show, :new, :create]
 end
+
+# Devise alert:
+
+# Some setup you must do manually if you haven't yet:
+
+#   Ensure you have overridden routes for generated controllers in your routes.rb.
+#   For example:
+
+#     Rails.application.routes.draw do
+#       devise_for :users, controllers: {
+#         sessions: 'users/sessions'
+#       }
+#     end
